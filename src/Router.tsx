851d341172bac61cc4966cec5d10176d058ddfc1
kyumin1227 <kyumin12271227@gmail.com>
2024-11-27 21:25:42 +0900

    #5.7: Route & InfoPage Style

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Coins from "./pages/Coins";
 import Coin from "./pages/Coin";
+import Chart from "./pages/Chart";
+import Price from "./pages/Price";
 
 const Router = createBrowserRouter([
   {
@@ -15,6 +17,16 @@ const Router = createBrowserRouter([
       {
         path: "/:coinId",
         element: <Coin />,
+        children: [
+          {
+            path: "chart",
+            element: <Chart />,
+          },
+          {
+            path: "price",
+            element: <Price />,
+          },
+        ],
       },
     ],
   },
--- src/pages/Coin.tsx
+++ src/pages/Coin.tsx
@@ -1,20 +1,119 @@
-import { useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Outlet, Route, Routes, useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
+import Chart from "./Chart";
+import Price from "./Price";
+
+interface InfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  logo: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  links: object;
+  links_extended: object;
+  whitepaper: object;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface PriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      price: number;
+      volume_24h: number;
+      volume_change_24h: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_12h: number;
+      percent_change_24h: number;
+      percent_change_7d: number;
+      percent_change_30d: number;
+      percent_change_1y: number;
+      ath_price: number;
+      ath_date: string;
+      percent_from_price_ath: number;
+    };
+  };
+}
 
 const Coin = () => {
   const { coinId } = useParams() as { coinId: string };
   const [loading, setLoading] = useState(true);
   const { state } = useLocation() as { state: { name: string } };
-  console.log(state);
+  const [info, setInfo] = useState<InfoData | undefined>();
+  const [price, setPrice] = useState<PriceData | undefined>();
+
+  useEffect(() => {
+    (async () => {
+      const infoData = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
+      const priceData = await (await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
+      setInfo(infoData);
+      setPrice(priceData);
+      setLoading(false);
+    })();
+  }, [coinId]);
 
   return (
     <>
       <Container>
         <Header>
-          <Title>{state?.name || "Loading"}</Title>
+          <Title>{state ? state.name : loading ? "Loading..." : info?.name}</Title>
         </Header>
-        {loading ? <Loader>Loading...</Loader> : null}
+        {loading ? (
+          <Loader>Loading...</Loader>
+        ) : (
+          <>
+            <Overview>
+              <OverviewItem>
+                <span>Rank:</span>
+                <span>{info?.rank}</span>
+              </OverviewItem>
+              <OverviewItem>
+                <span>Symbol:</span>
+                <span>{info?.symbol}</span>
+              </OverviewItem>
+              <OverviewItem>
+                <span>OPEN SOURCE:</span>
+                <span>{info?.open_source ? "Yes" : "No"}</span>
+              </OverviewItem>
+            </Overview>
+            <Description>{info?.description}</Description>
+            <Overview>
+              <OverviewItem>
+                <span>TOTAL SUPLY:</span>
+                <span>{price?.total_supply}</span>
+              </OverviewItem>
+              <OverviewItem>
+                <span>MAX SUPLY:</span>
+                <span>{price?.max_supply}</span>
+              </OverviewItem>
+            </Overview>
+          </>
+        )}
+        <Outlet />
       </Container>
     </>
   );
@@ -41,6 +140,31 @@ const Header = styled.header`
 const Loader = styled.span`
   text-align: center;
   display: block;
+`;
+
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  span: first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+
+const Description = styled.p`
+  margin: 20px 0px;
 `;
 
 export default Coin;
