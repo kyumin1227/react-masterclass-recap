47416a63d4e36e51d56f73781c4ed912c9c054b8
kyumin1227 <kyumin12271227@gmail.com>
2024-11-27 20:15:55 +0900

    #5.4: Route States

--- src/pages/Coin.tsx
+++ src/pages/Coin.tsx
@@ -1,8 +1,46 @@
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import styled from "styled-components";
 
 const Coin = () => {
-  const { coinId } = useParams();
-  return <>{coinId}</>;
+  const { coinId } = useParams() as { coinId: string };
+  const [loading, setLoading] = useState(true);
+  const { state } = useLocation() as { state: { name: string } };
+  console.log(state);
+
+  return (
+    <>
+      <Container>
+        <Header>
+          <Title>{state?.name || "Loading"}</Title>
+        </Header>
+        {loading ? <Loader>Loading...</Loader> : null}
+      </Container>
+    </>
+  );
 };
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
 
 export default Coin;
--- src/pages/Coins.tsx
+++ src/pages/Coins.tsx
@@ -1,59 +1,49 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const coins = [
-  { id: "btc-bitcoin", name: "Bitcoin", symbol: "BTC", rank: 1, is_new: false, is_active: true, type: "coin" },
-  { id: "eth-ethereum", name: "Ethereum", symbol: "ETH", rank: 2, is_new: false, is_active: true, type: "coin" },
-  { id: "usdt-tether", name: "Tether", symbol: "USDT", rank: 3, is_new: false, is_active: true, type: "token" },
-  { id: "sol-solana", name: "Solana", symbol: "SOL", rank: 4, is_new: false, is_active: true, type: "coin" },
-  {
-    id: "bnb-binance-coin",
-    name: "Binance Coin",
-    symbol: "BNB",
-    rank: 5,
-    is_new: false,
-    is_active: true,
-    type: "coin",
-  },
-  { id: "xrp-xrp", name: "XRP", symbol: "XRP", rank: 6, is_new: false, is_active: true, type: "coin" },
-  { id: "doge-dogecoin", name: "Dogecoin", symbol: "DOGE", rank: 7, is_new: false, is_active: true, type: "coin" },
-  { id: "usdc-usd-coin", name: "USDC", symbol: "USDC", rank: 8, is_new: false, is_active: true, type: "token" },
-  { id: "ada-cardano", name: "Cardano", symbol: "ADA", rank: 9, is_new: false, is_active: true, type: "coin" },
-  {
-    id: "steth-lido-staked-ether",
-    name: "Lido Staked Ether",
-    symbol: "STETH",
-    rank: 10,
-    is_new: false,
-    is_active: true,
-    type: "token",
-  },
-  { id: "avax-avalanche", name: "Avalanche", symbol: "AVAX", rank: 11, is_new: false, is_active: true, type: "coin" },
-  { id: "trx-tron", name: "TRON", symbol: "TRX", rank: 12, is_new: false, is_active: true, type: "coin" },
-  {
-    id: "toncoin-the-open-network",
-    name: "Toncoin",
-    symbol: "TON",
-    rank: 13,
-    is_new: false,
-    is_active: true,
-    type: "coin",
-  },
-];
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
 
 const Coins = () => {
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const res = await fetch("https://api.coinpaprika.com/v1/coins");
+      const json = await res.json();
+      setCoins(json.slice(0, 100));
+      setLoading(false);
+    })();
+  }, []);
+
   return (
     <Container>
       <Header>
         <Title>Coins</Title>
       </Header>
-      <CoinsList>
-        {coins.map((coin) => (
-          <Coin key={coin.id}>
-            <Link to={`/${coin.id}`}>{coin.name} &rarr; </Link>
-          </Coin>
-        ))}
-      </CoinsList>
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <CoinsList>
+          {coins.map((coin) => (
+            <Coin key={coin.id}>
+              <Link to={`/${coin.id}`} state={{ name: coin.name }}>
+                <Img src={`./node_modules/cryptocurrency-icons/svg/color/${coin.symbol.toLowerCase()}.svg`} />
+                {coin.name} &rarr;{" "}
+              </Link>
+            </Coin>
+          ))}
+        </CoinsList>
+      )}
     </Container>
   );
 };
@@ -65,6 +55,8 @@ const Title = styled.h1`
 
 const Container = styled.div`
   padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
 `;
 
 const Header = styled.header`
@@ -82,15 +74,27 @@ const Coin = styled.li`
   border-radius: 15px;
   margin-bottom: 10px;
   a {
+    display: flex;
+    align-items: center;
     padding: 20px;
     transition: color 0.2s ease-in;
-    display: block;
   }
   &:hover {
     a {
       color: ${(props) => props.theme.accentColor};
     }
   }
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
 `;
 
 export default Coins;
