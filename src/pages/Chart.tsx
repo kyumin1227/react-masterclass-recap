b3469c668d5da619344bcb3e624cea8b914eb5ed
kyumin1227 <kyumin12271227@gmail.com>
2024-11-29 22:01:34 +0900

    Code Challenge
    - gh-page publish

--- src/pages/Chart.tsx
+++ src/pages/Chart.tsx
@@ -14,21 +14,21 @@ interface ICoinHistory {
   market_cap: number;
 }
 
-function formatTimestamp(timestamp: number): string {
-  // 타임스탬프를 밀리초 단위로 변환
-  const date = new Date(timestamp * 1000);
+// function formatTimestamp(timestamp: number): string {
+//   // 타임스탬프를 밀리초 단위로 변환
+//   const date = new Date(timestamp * 1000);
 
-  // 날짜와 시간 구성 요소 추출
-  const year = date.getFullYear();
-  const month = (date.getMonth() + 1).toString().padStart(2, "0");
-  const day = date.getDate().toString().padStart(2, "0");
-  const hours = date.getHours().toString().padStart(2, "0");
-  const minutes = date.getMinutes().toString().padStart(2, "0");
-  const seconds = date.getSeconds().toString().padStart(2, "0");
+//   // 날짜와 시간 구성 요소 추출
+//   const year = date.getFullYear();
+//   const month = (date.getMonth() + 1).toString().padStart(2, "0");
+//   const day = date.getDate().toString().padStart(2, "0");
+//   const hours = date.getHours().toString().padStart(2, "0");
+//   const minutes = date.getMinutes().toString().padStart(2, "0");
+//   const seconds = date.getSeconds().toString().padStart(2, "0");
 
-  // 원하는 형식으로 조합
-  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-}
+//   // 원하는 형식으로 조합
+//   return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+// }
 
 const Chart = () => {
   const { state } = useLocation() as { state: { coinId: string } };
