78d97bcfb99fc89c9bbd44325283a237a084a8b9
kyumin1227 <kyumin12271227@gmail.com>
2024-12-05 23:55:24 +0900

    #8.2 Basic Animations

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,17 +4,20 @@ import { motion } from "framer-motion";
 const index = () => {
   return (
     <Wrapper>
-      <Box />
-      <motion.div></motion.div>
+      <Box
+        transition={{ type: "spring", mass: 1.3, delay: 0.5 }}
+        initial={{ scale: 0 }}
+        animate={{ scale: 1, rotateZ: 360 }}
+      />
     </Wrapper>
   );
 };
 
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
-  border-radius: 10px;
+  border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
