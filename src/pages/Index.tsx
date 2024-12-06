737db906fe1be79f268f082cc31275f3cf7c75c6
kyumin1227 <kyumin12271227@gmail.com>
2024-12-06 20:13:13 +0900

    #8.13: Slider

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,50 +3,67 @@ import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
 
 const boxVariants = {
-  initial: {
+  entry: (back: boolean) => ({
+    x: back ? -500 : 500,
     opacity: 0,
     scale: 0,
-  },
-  visible: {
+  }),
+  center: {
+    x: 0,
     opacity: 1,
     scale: 1,
-    rotateZ: 360,
   },
-  leaving: {
+  exit: (back: boolean) => ({
+    x: back ? 500 : -500,
     opacity: 0,
-    y: 20,
-  },
+    scale: 0,
+  }),
 };
 
 const index = () => {
-  const [showing, setShowing] = useState(false);
-  const toggleShowing = () => {
-    setShowing(!showing);
+  const [visible, setVisible] = useState(0);
+  const [back, setBack] = useState(false);
+  const toggleNext = () => {
+    setVisible((prev) => (prev + 1) % 10);
+    setBack(false);
+  };
+  const togglePrev = () => {
+    setVisible((prev) => (prev === 0 ? 9 : prev - 1));
+    setBack(true);
   };
 
   return (
     <Wrapper>
-      <button onClick={toggleShowing}>Click</button>
-      <AnimatePresence>
-        {showing ? (
-          <Box
-            variants={boxVariants}
-            initial="initial"
-            animate="visible"
-            exit="leaving"
-            transition={{ duration: 0.5 }}
-          />
-        ) : null}
+      <AnimatePresence custom={back}>
+        <Box
+          key={visible} // key가 변경되면 react는 새로운 컴포는트로 인식하여 애니메이션을 적용
+          custom={back}
+          variants={boxVariants}
+          initial="entry"
+          animate="center"
+          exit="exit"
+          transition={{ duration: 0.5 }}
+        >
+          {visible}
+        </Box>
       </AnimatePresence>
+      <button onClick={toggleNext}>Next</button>
+      <button onClick={togglePrev}>Prev</button>
     </Wrapper>
   );
 };
 
 const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
+  width: 150px;
+  height: 150px;
+  top: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background-color: white;
+  font-size: 20px;
   border-radius: 35px;
+  position: absolute;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
