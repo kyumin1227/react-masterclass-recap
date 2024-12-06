d12e48f224fe4e4a71a68e6b1a3797ea2e0dd802
kyumin1227 <kyumin12271227@gmail.com>
2024-12-06 18:25:28 +0900

    #8.9: MotionValues

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,49 +1,32 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
-import { useRef } from "react";
-
-const boxVariants = {
-  hover: {
-    rotateZ: 90,
-  },
-  click: {
-    borderRadius: "100px",
-  },
-  drag: {
-    backgroundColor: "rgb(46, 204, 113)",
-    transition: { duration: 10 },
-  },
-};
+import { motion, useMotionValue, useScroll, useTransform } from "framer-motion";
+import { useEffect } from "react";
 
 const index = () => {
-  const biggerBoxRef = useRef<HTMLDivElement>(null);
+  const x = useMotionValue(0);
+  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+  const gradient = useTransform(
+    x,
+    [-800, 0, 800],
+    [
+      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+      "linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238))",
+      "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
+    ]
+  );
+  const { scrollY, scrollYProgress } = useScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 3]);
+
+  useEffect(() => {
+    scrollY.onChange(() => console.log(scrollY.get(), scrollYProgress.get()));
+  }, [scrollY, scrollYProgress]);
+
   return (
-    <Wrapper>
-      <BiggerBox ref={biggerBoxRef}>
-        <Box
-          drag
-          dragSnapToOrigin
-          dragConstraints={biggerBoxRef}
-          variants={boxVariants}
-          whileHover="hover"
-          whileTap="click"
-          whileDrag="drag"
-        />
-      </BiggerBox>
+    <Wrapper style={{ background: gradient }}>
+      <Box style={{ x, rotateZ, scale }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 };
-
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.2);
-  border-radius: 40px;
-  overflow: hidden;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
 
 const Box = styled(motion.div)`
   width: 200px;
@@ -55,8 +38,8 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Wrapper = styled.div`
-  height: 100vh;
+const Wrapper = styled(motion.div)`
+  height: 200vh;
   width: 100vw;
   display: flex;
   justify-content: center;
